36ece973182d2d687082c925f4caee20d6fb51f1
OlivkaFromHell <dagerrashid@mail.ru>
2021-11-20 12:10:08 +0300

    rewrote hw5.py with slices

--- lecture_02_data_structures_functions/hw/hw5.py
+++ lecture_02_data_structures_functions/hw/hw5.py
@@ -18,15 +18,11 @@ from typing import Any, Iterable, List
 
 
 def custom_range(arr: Iterable[Any], start: Any, stop: Any = None, step: int = 1) -> List:
-    ans = []
+    # TODO: rewrite code with generators
     arr = list(arr)
     ind_start = arr.index(start)
     if not stop:
-        for i in range(ind_start):
-            ans.append(arr[i])
+        return arr[:ind_start]
     else:
         ind_stop = arr.index(stop)
-        for i in range(ind_start, ind_stop, step):
-            ans.append(arr[i])
-
-    return ans
+        return arr[ind_start:ind_stop:step]
