7197b8456b408ec417a590d8ae8edaa68ac85c1c
OlivkaFromHell <dagerrashid@mail.ru>
2021-11-26 16:46:50 +0300

    update main.yml and tests path

--- lecture_04_test/tests/test_task_1_read_file.py
+++ lecture_04_test/tests/test_task_1_read_file.py
@@ -3,6 +3,8 @@ import os
 import pytest
 from hw.task_1_read_file import read_magic_number
 
+cwd = os.path.dirname(os.path.realpath(__file__)) + '/'
+
 
 @pytest.mark.parametrize("path, text", [
     ('test1.txt', '2\nHi, fellows\nisdigit'),
@@ -11,6 +13,7 @@ from hw.task_1_read_file import read_magic_number
     ('test3.txt', '2.999999\n3\n5'),
 ])
 def test_positive_case(path, text):
+    path = cwd + path
     with open(path, 'w') as f:
         f.write(text)
     assert read_magic_number(path) is True
@@ -25,6 +28,7 @@ def test_positive_case(path, text):
     ('test.txt', '1e4\n'),
 ])
 def test_negative_case(path, text):
+    path = cwd + path
     with open(path, 'w') as f:
         f.write(text)
     assert read_magic_number(path) is False
@@ -38,6 +42,7 @@ def test_negative_case(path, text):
     ('test.txt', 'python4.999999'),
 ])
 def test_with_error(path, text):
+    path = cwd + path
     with open(path, 'w') as f:
         f.write(text)
     with pytest.raises(ValueError):
