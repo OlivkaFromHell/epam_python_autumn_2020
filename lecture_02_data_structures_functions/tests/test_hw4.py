d36810807085156384d89dadd7c0227daaabaa2e
OlivkaFromHell <dagerrashid@mail.ru>
2021-11-28 16:04:53 +0300

    update test_hw4.py.

--- lecture_02_data_structures_functions/tests/test_hw4.py
+++ lecture_02_data_structures_functions/tests/test_hw4.py
@@ -16,64 +16,84 @@ def test_positive_case1():
 
 
 def test_positive_case2():
+    check = True
+
     def func(a, b):
+        nonlocal check
+        check = not check
         return a + b
 
     cache_func = cache(func)
 
     some = 'import', 'this'
 
-    val_1 = cache_func(*some)
-    val_2 = cache_func(*some)
+    cache_func(*some)
+    cache_func(*some)
 
-    assert val_1 is val_2
+    assert not check
 
 
 def test_positive_case3():
+    check = True
+
     def func(repeat: str, amount: int):
+        nonlocal check
+        check = not check
         return repeat * amount
 
     cache_func = cache(func)
 
     some = 'zzz', 3
 
-    val_1 = cache_func(*some)
-    val_2 = cache_func(*some)
+    cache_func(*some)
+    cache_func(*some)
 
-    assert val_1 is val_2
+    assert not check
 
 
 def test_args_order():
+    check = True
+
     def func(a: int, b: int):
+        nonlocal check
+        check = not check
         return a - b
 
     cache_func = cache(func)
 
-    val_1 = cache_func(3, 2)
-    val_2 = cache_func(2, 3)
+    cache_func(3, 2)
+    cache_func(2, 3)
 
-    assert val_1 is not val_2
+    assert check
 
 
 def test_kwargs_order():
+    check = True
+
     def func(a: int, b: int):
+        nonlocal check
+        check = not check
         return a - b
 
     cache_func = cache(func)
 
-    val_1 = cache_func(a=3, b=2)
-    val_2 = cache_func(b=2, a=3)
+    cache_func(a=3, b=2)
+    cache_func(b=2, a=3)
 
-    assert val_1 is val_2
+    assert not check
 
 
 def test_args_kwargs_name():
+    check = True
+
     def func(a: str, b: str):
+        nonlocal check
+        check = not check
         return a + b
 
     cache_func = cache(func)
 
-    val_1 = cache_func('2', b='\'b\'')
-    val_2 = cache_func(a='2', b='\'b\'')
+    cache_func('2', b='\'b\'')
+    cache_func(a='2', b='\'b\'')
 
-    assert val_1 is val_2
+    assert not check
